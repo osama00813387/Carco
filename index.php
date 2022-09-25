fb5dab871aaa92c3bbb92e3c7e74c902e906f3eb
osama00813387 <114333489+osama00813387@users.noreply.github.com>
2022-09-25 11:37:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CarCo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    </head>
+    <body>
+    
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <button type="button" class="btn btn-dark">CARCO</button>
+
+    <div class="container-fluid">
+         
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#">PRODUCTS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CURRENT SALES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">MEMBER+</a>
+      </li>       
+    </ul>
+    <div class="d-grid gap-2 d-md-block">
+
+    <button type="button" class="btn btn-dark">SIGN UP</button>
+
+    <button type="button" class="btn btn-dark">LOGIN</button>
+
+
+</div>
+  </div>
+
+ </nav>
+       </header>
+    </body>
+    </html>
